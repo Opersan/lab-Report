b717c65946c56c74bd1c024a47b225c1f536841e
Furkan Kiraz <furkankirazmp@gmail.com>
2022-05-13 00:16:37 +0300

    Some changes

--- src/main/java/com/example/labreport/controller/MainController.java
+++ src/main/java/com/example/labreport/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controller
+public class MainController
 {
     @GetMapping("/")
     public String homePage()
